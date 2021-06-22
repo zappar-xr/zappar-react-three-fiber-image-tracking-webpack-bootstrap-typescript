90c977cca8f73a2560f27b3fb2fcab8e92513a77
Zappar <hello@zappar.com>
2021-06-22 17:38:05 +0100

    Minimal webpack project for Zappar react-three-fiber

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,24 +1,83 @@
-/* 
- declarations.d.ts
- This file allows us to use import statement to import files with the listed extensions 
+/*
+ This file allows us to use import statement to import files with the listed extensions
 */
-declare module "*.zpt"
-declare module "*.png"
-declare module "*.jpg"
-declare module "*.gif"
-declare module "*.jpeg"
-declare module "*.glb"
-declare module "*.gltf"
-declare module "*.ogg"
-declare module "*.mp3"
-declare module "*.obj"
-declare module "*.fbx"
-declare module "*.wav"
-declare module "*.ttf"
-declare module "*.fnt"
-declare module "*.woff"
-declare module "*.stl"
-declare module "*.mp4"
-declare module "*.json"
-declare module "*.hdr"
-declare module "*.webm"
+declare module "*.zpt" {
+  const content: string;
+  export default content;
+}
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+declare module "*.glb" {
+  const content: string;
+  export default content;
+}
+declare module "*.gltf" {
+  const content: string;
+  export default content;
+}
+declare module "*.ogg" {
+  const content: string;
+  export default content;
+}
+declare module "*.mp3" {
+  const content: string;
+  export default content;
+}
+declare module "*.obj" {
+  const content: string;
+  export default content;
+}
+declare module "*.fbx" {
+  const content: string;
+  export default content;
+}
+declare module "*.wav" {
+  const content: string;
+  export default content;
+}
+declare module "*.ttf" {
+  const content: string;
+  export default content;
+}
+declare module "*.fnt" {
+  const content: string;
+  export default content;
+}
+declare module "*.woff" {
+  const content: string;
+  export default content;
+}
+declare module "*.stl" {
+  const content: string;
+  export default content;
+}
+declare module "*.mp4" {
+  const content: string;
+  export default content;
+}
+declare module "*.json" {
+  const content: string;
+  export default content;
+}
+declare module "*.hdr" {
+  const content: string;
+  export default content;
+}
+declare module "*.webm" {
+  const content: string;
+  export default content;
+}
